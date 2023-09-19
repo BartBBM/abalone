c7fc22a6ce892164e4d1093bee5e38142b8a0ebc
Bebbo <bartolomeo.mueller@stud.h-da.de>
2023-09-19 13:14:57 +0200

    updated crossfade animation

--- src/lib/utils/crossfade.ts
+++ src/lib/utils/crossfade.ts
@@ -2,21 +2,21 @@ import { quintOut } from 'svelte/easing';
 import { crossfade as svelteCrossfade } from 'svelte/transition';
 
 export const crossfade = svelteCrossfade({
-	duration: (d) => Math.sqrt(d * 10000),
+	duration: (d) => Math.sqrt(d * 10000)
 
-	fallback(node, params) {
-		const style = getComputedStyle(node);
-		const transform = style.transform === 'none' ? '' : style.transform;
+	// fallback(node, params) {
+	// 	const style = getComputedStyle(node);
+	// 	const transform = style.transform === 'none' ? '' : style.transform;
 
-		return {
-			duration: 600,
-			easing: quintOut,
-			css: (t) => `
-					transform: ${transform} scale(${t});
-					opacity: ${t}
-				`
-		};
-	}
+	// 	return {
+	// 		duration: 500,
+	// 		easing: quintOut,
+	// 		css: (t) => `
+	// 				transform: ${transform} scale(${t});
+	// 				opacity: ${t}
+	// 			`
+	// 	};
+	// }
 });
 
 // export const crossfade = svelteCrossfade({});
